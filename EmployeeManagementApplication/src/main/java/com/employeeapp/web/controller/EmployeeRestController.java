8ecaccbf17295f426754e7be713ac4ab0310ebbf
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-19 14:59:39 +0530

    first commit

--- EmployeeManagementApplication/src/main/java/com/employeeapp/web/controller/EmployeeRestController.java
+++ EmployeeManagementApplication/src/main/java/com/employeeapp/web/controller/EmployeeRestController.java
@@ -18,30 +18,30 @@ import com.employeeapp.model.service.EmployeeService;
 public class EmployeeRestController {
 	private EmployeeService employeeservice;
     @Autowired
-	public EmployeeRestController(EmployeeService employeeservice) {
+    public EmployeeRestController(EmployeeService employeeservice) {
 		super();
 		this.employeeservice = employeeservice;
 	}
     
     @GetMapping(path = "/employee", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Employee>> getAllEmployees() {
+    public ResponseEntity<List<Employee>> getAllEmployees() {
 		return new ResponseEntity<List<Employee>>(employeeservice.findAll(), HttpStatus.OK);
 	}
 
     @GetMapping(path = "/employee/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Employee> getAnEmployee(@PathVariable(name = "id") Long id) {
+    public ResponseEntity<Employee> getAnEmployee(@PathVariable(name = "id") Long id) {
 		Employee employee = employeeservice.findEmployeeById(id).orElseThrow(EmployerNotFoundException::new);
 		return new ResponseEntity<Employee>(employee, HttpStatus.OK);
 	}
 	
     @GetMapping(path="/employee/limit/{salary}",produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Employee>> getBySalaryLimit(@PathVariable(name="salary")double salary){
+    public ResponseEntity<List<Employee>> getBySalaryLimit(@PathVariable(name="salary")double salary){
 		return new ResponseEntity<List<Employee>>(employeeservice.salaryLimit(salary),HttpStatus.OK);
 	}
 	
     @GetMapping(path = "/employeeName/{firstName}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Employee> getAnEmployee(@PathVariable(name = "firstName") String firstName) {
-        Employee employee = employeeservice.findByfirstName(firstName).orElseThrow(EmployeeNotFoundException::new);
-        return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+    public ResponseEntity<Employee> getAnEmployee(@PathVariable(name = "firstName") String firstName) {
+               Employee employee = employeeservice.findByfirstName(firstName).orElseThrow(EmployeeNotFoundException::new);
+               return new ResponseEntity<Employee>(employee, HttpStatus.OK);
     }
 }
