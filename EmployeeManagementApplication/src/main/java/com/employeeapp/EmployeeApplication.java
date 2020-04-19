ea82b4cf3af44d167fbb826bcee550509a89e920
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-04-19 15:44:33 +0530

    first commit

--- EmployeeManagementApplication/src/main/java/com/employeeapp/EmployeeApplication.java
+++ EmployeeManagementApplication/src/main/java/com/employeeapp/EmployeeApplication.java
@@ -16,7 +16,7 @@ public class EmployeeApplication implements CommandLineRunner {
 	private EmployeeRepository employeeRepository;
     public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
-	}
+    }
     @Override
     public void run(String... args) throws Exception {
 		/*
